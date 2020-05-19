ea648b2e446d48cfbb0547f234792aa70632b7da
Bruno Teixeira <bosih50890@emailhost99.com>
2020-05-19 16:04:25 +0200

    added frontend and backend folders

---
+++ Backend/src/main/java/com/mobilecomputing/pecunia/PecuniaApplication.java
@@ -0,0 +1,13 @@
+package com.mobilecomputing.pecunia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecuniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PecuniaApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/mobilecomputing/pecunia/PecuniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilecomputing.pecunia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecuniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
