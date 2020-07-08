f0c83e95efa4acca3b21a8b5c300a7a3308899ac
Slimexsan <Filip.Stanicak@Student.Reutlingen-University.de>
2020-07-09 01:01:22 +0200

    Added Emailservice to lost pw screen

--- Frontend/app/src/main/java/com/example/mobileapp/Lost_PW.java
+++ Frontend/app/src/main/java/com/example/mobileapp/Lost_PW.java
@@ -4,18 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Random;
 
 
 public class Lost_PW extends AppCompatActivity {
 
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputCode;
+    private String email;
+    private Button sendEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +30,37 @@ public class Lost_PW extends AppCompatActivity {
 
         textInputEmail = findViewById(R.id.text_input_email);
         textInputCode = findViewById(R.id.text_input_reset_code);
+        sendEmail = findViewById(R.id.send_email_button);
+
+        sendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!validateEmail()) {
+                    return;
+                }
+                Random rand = new Random();
+                int codeInt = rand.nextInt(999999);
+                String code = String.format("%06d", codeInt);
+                textInputEmail.setEnabled(false);
+                textInputEmail.setEndIconVisible(false);
+                Toast.makeText(getApplicationContext(), "Sending E-Mail ...", Toast.LENGTH_SHORT).show();
+                try {
+                    GMailSender sender = new GMailSender();
+                    sender.sendMail("Password reset code",
+                            "This is your 6 Digit Code: \n" +
+                            code, "stanicakfilip@gmail.com", "stanicakfilip@gmail.com");
+                } catch (Exception e) {
+                    Log.e("SendMail", e.getMessage(), e);
+                }
+            }
+        });
 
     }
 
     private boolean validateEmail() {
-        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+        email = textInputEmail.getEditText().getText().toString().trim();
 
-        if (emailInput.isEmpty()) {
+        if (email.isEmpty()) {
             textInputEmail.setError("Field cannot be empty");
             return false;
         } else {
@@ -62,15 +92,6 @@ public class Lost_PW extends AppCompatActivity {
             startActivity(new Intent(Lost_PW.this, New_PW_Screen.class));
         }
 
-    }
-
-    public void sendEmail(View v) {
-        if (!validateEmail()) {
-            return;
-        }
-        textInputEmail.setEnabled(false);
-        textInputEmail.setEndIconVisible(false);
-        Toast.makeText(this, "Sending E-Mail ...", Toast.LENGTH_SHORT).show();
     }
 
     public void backButton(View view) {
