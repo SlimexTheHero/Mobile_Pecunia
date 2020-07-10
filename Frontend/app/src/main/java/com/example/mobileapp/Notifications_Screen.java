6bc619ba6b09f9c3c3e4f2fcbddfa0346f37ae91
Slimexsan <Filip.Stanicak@Student.Reutlingen-University.de>
2020-07-10 16:15:24 +0200

    Added Notifications

--- Frontend/app/src/main/java/com/example/mobileapp/Notifications_Screen.java
+++ Frontend/app/src/main/java/com/example/mobileapp/Notifications_Screen.java
@@ -1,13 +1,25 @@
 package com.example.mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+
+import static com.example.mobileapp.Notification_Popup.CHANNEL_1_ID;
+import static com.example.mobileapp.Notification_Popup.CHANNEL_2_ID;
 
 public class Notifications_Screen extends AppCompatActivity {
 
@@ -15,6 +27,9 @@ public class Notifications_Screen extends AppCompatActivity {
     private ArrayList<String> notificationTitle = new ArrayList<>();
     private ArrayList<String> notificationGroup = new ArrayList<>();
     private ArrayList<String> notificationMessage = new ArrayList<>();
+    private TextView testButtonOne;
+    private TextView testButtonTwo;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,64 @@ public class Notifications_Screen extends AppCompatActivity {
 
         initNotifications();
 
+        notificationManager = NotificationManagerCompat.from(this);
+
+        testButtonOne = findViewById(R.id.notifications_test_one);
+        testButtonTwo = findViewById(R.id.notifications_test_two);
+
+        testButtonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendOnChannel1();
+            }
+        });
+
+        testButtonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendOnChannel2();
+            }
+        });
+
+    }
+
+    public void sendOnChannel1() {
+
+        Intent activityIntent = new Intent(this, Notifications_Screen.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_1_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("New Transaction oder Delete Transaction") //TODO Wird durch IF Abfrage nach Typ geändert
+                .setContentText("Gruppenname")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Hier steht die gesamte Transaktion als Notification. Hier steht so viel scheiße drinnen, da es nicht komplett angezeigt werden kann.") //TODO
+                        .setBigContentTitle("Hier kommt der Gruppenname hin") //TODO
+                        .setSummaryText("Transaction"))
+                .setColor(Color.parseColor("#C5B358"))
+                .setContentIntent(contentIntent)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void sendOnChannel2() {
+        Intent activityIntent = new Intent(this, Notifications_Screen.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this,
+                0, activityIntent, 0);
+        Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_2_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Admin, Removed or Added to Trip") //TODO Wird durch IF Abfrage nach Typ geändert
+                .setContentText("Gruppenname")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Hier steht ob Gruppe hinzugekommen, Admin geworden oder von einer Grupp entfernt") //TODO
+                        .setBigContentTitle("Hier kommt der Gruppenname hin") //TODO
+                        .setSummaryText("Trip"))
+                .setColor(Color.parseColor("#C5B358"))
+                .setContentIntent(contentIntent)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 
     private void initNotifications() {
@@ -63,7 +136,7 @@ public class Notifications_Screen extends AppCompatActivity {
     }
 
 
-    public void backButton (View v) {
+    public void backButton(View v) {
         finish();
     }
 }
